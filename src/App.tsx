12d789c19b3089e6ad5fc5cc0c7cb11a265c0e50
Alan Francis <alan.prime567@gmail.com>
2024-12-11 23:58:52 +0530

    feat:add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import { Button } from "./components/ui/button";
+import NotFound from "./pages/notfound";
+import Layout from "./layouts";
+import Landing from "./pages/landing";
 
 function App() {
-  return (
-    <>
-      <Button>hi</Button>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+    {
+      path: "/404",
+      element: <NotFound />,
+    },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Landing />,
+        },
+      ],
+    },
+    // {
+    //   path: "/",
+    //   element: <BusinessLayout />,
+    //   children: [
+    //     {
+    //       path: "business",
+    //       element: <Business />,
+    //     },
+    //     {
+    //       path: "realestate",
+    //       element: <RealEstate />,
+    //     },
+    //     {
+    //       path: "marketing",
+    //       element: <Marketing />,
+    //     },
+    //   ],
+    // },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+
+const Landing = () => {
+  return (
+    <div className="bg-primary h-screen flex flex-col gap-8 justify-between">
+      <div className="text-white font-PlusJakartaSans p-8 font-medium">
+        <h3>Hey</h3>
+        <h3>You still trying to figure out what μlearn </h3>
+        <h3>is? Check this out 👇</h3>
+        <div className="px-5 mt-8">
+          <iframe
+            className="w-full h-52 rounded-3xl"
+            src="https://www.youtube.com/embed/M9serw-CLU0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+      <div className="bg-white text-black relative py-16 px-5 gap-6 flex flex-col h-[55%] justify-center items-center text-center tracking-tighter rounded-t-3xl">
+        <img src="/Logo.svg" alt="" height={100} width={100} />
+        <img
+          src="/common/planet.webp"
+          alt=""
+          height={90}
+          width={90}
+          className="absolute top-3 right-3"
+        />
+        <div className="w-5/6">
+          <h1 className="text-3xl font-bold tracking-tighter text-nowrap">
+            Learn Skills In No Time
+          </h1>
+          <h3 className="text-lg font-normal mt-2">
+            Learn, grow, and connect with a community of passionate learners.
+            Unlock your full potential together.
+          </h3>
+        </div>
+        <div className="mt-16">
+          <Button className="w-full text-lg py-8 rounded-full px-28">
+            Discover Who You Are
+          </Button>
+          <p className="text-lg mt-4 text-primary">
+            Already have an account?{" "}
+            <a href="#" className="text-xl font-semibold">
+              Sign In
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
